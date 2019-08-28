99db14ac217fce78cc1b0f2fe5e5674269a862aa
fatoni <53787730+fatonigans@users.noreply.github.com>
2019-08-28 20:50:34 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,103 +1,233 @@
-#!/bin/sh
-#version 1.0
-# Variables
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
-blue='\e[1;34m'
-cyan='\e[1;36m'
-red='\e[1;31m'
-
-figlet fatoni | lolcat
-
-echo   "=======================" |lolcat
-echo   "Tools    : mr.black $white         " |lolcat
-echo   "Creadby  : fatonigans   " |lolcat
-echo   "Contact  : wa:085691015635 " |lolcat
-echo   "======================="|lolcat
-ulang='y'
-while [ $ulang = 'y' ];
-do
-  echo '\033[0;36m1) install darkfbv1.7'
-echo   "=======================" |lolcat
-  echo '\33[1;33m2) install BRUTEFORCEnew'
-echo   "=======================" |lolcat
-  echo '\33[31;1m3) install LazyGo'
-echo   "=======================" |lolcat
-  echo '\33[0;32m0) exit '
-echo   "=======================" |lolcat
+#!/bin/bash
+clear
+bi='\033[34;1m' #BIRU
+i='\033[32;1m' #IJO
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+me='\033[31;1m' #MERAH
+pu='\033[37;1m' #PUTIH
+ku='\033[33;1m' #KUNING
+echo
+echo
+clear
+echo
+figlet tools|lolcat
 sleep 1
-  read -p 'Masukkan Pilhan anda : ' pil;
-  if [ $pil = '1' ]
-  then
-      echo 'install darkfbv1.7'|lolcat
-      echo 'sabar bangsat'|lolcat
-      cd $HOME
-     apt update && apt upgrade -y
-
-pkg install git
-
-pkg install python2
-
+echo
+echo
+clear
+echo
+figlet fatoni|lolcat
+sleep 1
+echo
+echo
+echo
+echo
+clear
+figlet mr.black|lolcat
+echo
+echo $me"=========="$ku"=========="$i"=========="
+echo $me"█"$cy"   author :mr.black        "             $me"█"
+echo $ku"█"$cy"   whatsapp:085691015635   "            $ku"█"
+echo $i"█"$cy"   facebook:tools termux    "                $i"█"
+echo $me"=========="$ku"=========="$i"=========="
+echo
+echo $i"-----› Menu BRUTEPORCE NEWS:"
+echo $cy"⟨"$pu"1"$cy"⟩"$pur" -» DarkFb v1.7"
+echo $cy"⟨"$pu"2"$cy"⟩"$cy" -» tools moreno 77"
+echo $cy"⟨"$pu"3"$cy"⟩"$bi" -» spam sms"
+echo $cy"⟨"$pu"4"$cy"⟩"$bi" -» bruteforce fb"
+echo $cy"⟨"$pu"5"$cy"⟩"$bi" -» mbf "
+echo $cy"⟨"$pu"6"$cy"⟩"$bi" -» OsiF"
+echo $cy"⟨"$pu"7"$cy"⟩"$bi" -» Auto password multi bruteforce"
+echo $cy"⟨"$pu"8"$cy"⟩"$bi" -» hack instagram"
+echo $cy"⟨"$pu"9"$cy"⟩"$bi" -» bot facebook"
+echo $cy"⟨"$pu"10"$cy"⟩"$bi"-» hack wifi (root)"
+echo $cy"⟨"$pu"11"$cy"⟩"$bi"-» install webdav"
+echo $cy"⟨"$pu"12"$cy"⟩"$bi"-» install redhawk"
+echo $cy"⟨"$pu"13"$cy"⟩"$bi"-» install lite DDOS"
+echo $cy"⟨"$pu"14"$cy"⟩"$bi"-» install tools B4J1N64N "
+echo $cy"⟨"$pu"15"$cy"⟩"$bi"-» tembak XL"
+echo $cy"⟨"$pu"16"$cy"⟩"$bi"-» Keluar"
+echo
+echo $pu"╭─["$i"Pilih sesuai ke inginan Anda ya bangsat"$pu"]"
+read -p"╰─> : " pil
+echo
+if [ $pil = 1 ]
+then
+clear
 git clone https://github.com/storiku/darkfb
-
 pip2 install requests
-
 pip2 install mechanize
-
 cd darkfb
-
 python2 Dark.py
-      echo 'installing succes....! '
-      sleep 1
-  elif [ $pil = '2' ];
-  then
-      echo 'install BRUTEFORCEnew '|lolcat
-      cd $HOME
-   pkg update && pkg upgrade
+fi
 
+if [ $pil = 2 ]
+then
+clear
+ git clone https://github.com/DarkCurut08/DarkCurut08
+ cd DarkCurut08
+chmod +x Moreno77.sh
+sh Moreno77.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/joss24242/SpamSms
+cd SpamSms
+python2 mantan.py
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/Senitopeng/fbbrute
+cd fbbrute
+python2 jomblo.py
+fi
+
+if [ $pil = 5 ]
+then
+clear
+https://github.com/pirmansx/mbf
+pip2 install mechanize
+cd mbf
+python2 mbf.py
+fi
+
+if [ $pil = 6 ]
+then
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+clear
+fi
+
+if [ $pil = 7 ]
+then
+ pkg update && pkg upgrade
+ pkg install python2
+ pip2 install requests
+pip2 install mechanize
  pkg install git
+ git clone https://github.com/ARIYA-CYBER/NEW
+ cd NEW
+ python2 NEW.py
+clear
+fi
 
- pkg install python
+if [ $pil = 8 ]
+then
+ apt update && apt upgrade -y
+ apt install nodejs git
+ cd Instagram-Private-Tools
+ node index.js
+ git clone https://github.com/ccocot/Instagram-Private-Tools.git
+ npm install
+ fi
+ 
+ if [ $pil = 9 ]
+ then
+ git clone https://github.com/Senitopeng/BotFbBangDjon.git
+ cd BotFbBangDjon
+ python2 bangdjon.py
+ fi
+ 
+ if [ $pil = 10 ]
+ then
+ apt update
+apt upgrade
+apt install git
+git clone https://github.com/esc0rtd3w/wifi-hacker
+ls
+cd wifi-hacker
+ls
+chmod +x wifi-hacker.sh
+ls
+./wifi-hacker.sh
+fi
 
-pip install requests
+if [ $pil = 11 ]
+then
+ apt update && upgrade
+ apt install python2
+ pip2 install urllib3 chardet certifi idna requests
+ apt install openssl curl
+ pkg install libcurl
+ ln -s /sdcard
+ cd sdcard
+ mkdir webdav
+cd webdav
+fi
 
- pip install bs4
+if [ $pil = 12 ]
+then 
+ apt update
+ apt install git
+ git clone https://github.com/Tuhinshubhra/RED_HAWK
+ cd RED_HAWK
+ chmod +x rhawk.php
+ apt install php
+ ls
+ php rhawk.php
+ fi
+ 
+ if [ $pil = 13 ]
+ then
+apt update
+ apt upgrade
+ pkg install git
+ pkg install python2
+ git clone https://github.com/4L13199/LITEDDOS
+ cd LITEDDOS
+python2 liteDDOS.py
+fi
 
- git clone https://github.com/FR13ND8/BruteFb
+if [ $pil = 14 ]
+then
+pkg install update
+pkg install git
+pkg install toilet
+pkg install figlet
+pip2 install lolcat
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5
+cd B4J1N64Nv5
+sh B4J1N64N.sh
+fi
 
- cd BruteFb
+if [ $pil = 15 ]
+then
+pkg upgrade
+pkg update
+pkg install git
+pkg install python
+git clone https://github.com/albertoanggi/xl-py
+pip install -r requirements.txt
+chmod +x app.py
+python/python2 app.py
+fi
 
- python fb.py
-      echo 'installing succes...! '
-      sleep 1
-  elif [ $pil = '3' ];
-  then
-      echo 'installing LazyGo'
-      cd $HOME
-      apt update && apt upgrade -y
-      apt install git
-      git clone https://github.com/kadal15/LazyGo.git
-      cd LazyGo
-      sh install.sh
-      echo 'installing succes.. '
-      sleep 1
-  elif [ $pil = '0' ];
-  then
-      echo ''
-      sleep 2
-      exit
-  else
-      echo 'salah goblok ' |lolcat
-      sleep 1
-      echo $ulang
-  fi
-done
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $i"kalian itu ganteng"
+echo $i"cuman mukanya buriq"
+sleep 2
+echo $i"Silahkan menikmati goyanganku wkkw"
+sleep 2
+echo $i"Buat Akang²"
+sleep 2
+echo $i"Bila Akang belum mengerti Akang Bisa Nanya"
+echo $i"Melalui Via:"
+sleep 2
+echo $ku"Facebook :"$pur" fatoni"
+echo $ku"YouTube  :"$pur" mr.black"
+echo $ku"WhatsApp :"$pur" 085691015635"
+clear
+exit
+fi
